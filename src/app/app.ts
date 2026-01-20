fbf6cf2c27ff43604bc450af38708efabd03a96d
Himapriyapothuganti <phimapriya600@gmail.com>
2026-01-20 12:47:44 +0530

    Add Angular Day 15 assignment

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Products } from '../components/products/products';
+import { NavBar } from '../components/nav-bar/nav-bar';
+import { Description } from '../components/description/description';
+import { Banner } from '../components/banner/banner';
+import { Cards } from '../components/cards/cards';
+import { Footer } from '../components/footer/footer';
+ 
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Products,NavBar,Description,Banner,Cards,Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
