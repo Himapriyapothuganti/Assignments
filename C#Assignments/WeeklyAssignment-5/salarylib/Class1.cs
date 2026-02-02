da40787b042f76b7236dca6aea5eb446df64f19f
Himapriya Pothuganti <155510905+Himapriyapothuganti@users.noreply.github.com>
2026-02-02 14:36:11 +0530

    Add files via upload

---
+++ C#Assignments/WeeklyAssignment-5/salarylib/Class1.cs
@@ -0,0 +1,27 @@
+﻿namespace salarylib
+{
+    public class Class1
+    {
+        public static double cal(double basicSalary)
+        {
+          
+  
+                if (basicSalary <= 0)
+                {
+                    Console.WriteLine("Invalid salary");
+                throw new Exception("Invalid Salary");
+                }
+
+                double hra = 0.20 * basicSalary;
+                double da = 0.10 * basicSalary;
+                double pf = 0;
+
+                if (basicSalary >= 15000)
+                    pf = 0.12 * basicSalary;
+
+                return basicSalary + hra + da - pf;
+            
+
+        }
+    }
+}
