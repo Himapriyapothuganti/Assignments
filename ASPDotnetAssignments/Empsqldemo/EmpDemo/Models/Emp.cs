b346032c530c02076cc4145fd33c3e25ad10ab56
Himapriyapothuganti <phimapriya600@gmail.com>
2026-02-17 11:46:43 +0530

    Employee sqlConnection demo

---
+++ ASPDotnetAssignments/Empsqldemo/EmpDemo/Models/Emp.cs
@@ -0,0 +1,9 @@
+﻿namespace EmpDemo.Models
+{
+    public class Emp
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
---
+++ ASPDotnetAssignments/Empsqldemo/EmpDemo/Models/EmpDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmpDemo.Models
+{
+    public class EmpDbContext : DbContext
+    {
+        //constructor injection
+        public EmpDbContext(DbContextOptions<EmpDbContext> options)
+             : base(options) { }
+        public DbSet<Emp> Emps { get; set; }
+    }
+}
