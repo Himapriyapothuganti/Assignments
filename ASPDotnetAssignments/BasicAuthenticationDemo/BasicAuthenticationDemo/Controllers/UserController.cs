226d8cc74f6a78ebdf1b4491660c7092651654d1
Himapriyapothuganti <phimapriya600@gmail.com>
2026-02-20 09:32:29 +0530

    Authentication demo

---
+++ ASPDotnetAssignments/BasicAuthenticationDemo/BasicAuthenticationDemo/Controllers/UserController.cs
@@ -0,0 +1,72 @@
+﻿using BasicAuthenticationDemo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BasicAuthenticationDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AuthContext _context;
+
+        public UserController(AuthContext context)
+        {
+            _context = context;
+        }
+        [HttpPost("Register")]
+
+        public ActionResult<User> Register(UserDTO userdto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var user = new User
+            {
+                FirstName = userdto.FirstName,
+                LastName = userdto.LastName,
+                Email = userdto.Email,
+                Password = userdto.Password
+            };
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+            return Ok(user);
+
+        }
+
+        [HttpPost("Login")]
+        public IActionResult Login(LoginDTO logindto)
+        {
+            var validUser = _context.Users.FirstOrDefault(u => u.Email == logindto.Email && u.Password == logindto.Password && u.IsActive == true);
+            if (validUser == null)
+                return NotFound();
+            return Ok(validUser);
+
+
+        }
+
+
+        [HttpGet("{id}")]
+        public ActionResult GetUserprofile(int id)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.UserId == id);
+            if (user == null)
+                return NotFound();
+            return Ok(user);
+        }
+
+        [HttpGet("{email}")]
+        public ActionResult GetUserbyEmail (string email)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Email == email);
+            if(user == null)
+                return NotFound();
+            return Ok(user);
+        }
+    }
+}
---
+++ ASPDotnetAssignments/BasicAuthenticationDemo/BasicAuthenticationDemo/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace BasicAuthenticationDemo.Models
+{
+    public class User {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsActive { get; set; }=true;
+        public DateTime createdon { get; set; } = DateTime.Now;
+
+
+    }
+}
